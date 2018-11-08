4103bd1e5f3b9e9af7622e87998f878cd22023fa
shinsoyeon <shinsoyeon16@yi.ackr>
2018-11-08 14:22:16 +0900

    1108

---
+++ lesson1108/Cat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1108
+{
+    class Cat
+    {
+       string Name = " ";
+        string Color =" ";
+        public Cat(string _Name, string _Color) { Name = _Name; Color = _Color; }
+        ~Cat()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lesson1108/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1108
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat haha = new Cat();
+            Cat kitty = new Cat("kitty", "하얀색");
+            kitty.Meow();
+        }
+    }
+}
