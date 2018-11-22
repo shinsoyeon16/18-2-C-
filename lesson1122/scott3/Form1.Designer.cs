1038dd0455df790f7ea8d55b688fae51a57b2bac
shinsoyeon <shinsoyeon16@yi.ackr>
2018-11-22 15:20:38 +0900

    1122

---
+++ lesson1122/scott3/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace scott3
+{
+    public partial class Form1 
+    {
+       
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.empo = new System.Windows.Forms.Label();
+            this.noTxt = new System.Windows.Forms.TextBox();
+            this.salTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 104);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(458, 325);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // empo
+            // 
+            this.empo.AutoSize = true;
+            this.empo.Location = new System.Drawing.Point(23, 13);
+            this.empo.Name = "empo";
+            this.empo.Size = new System.Drawing.Size(44, 12);
+            this.empo.TabIndex = 2;
+            this.empo.Text = "empno";
+            // 
+            // noTxt
+            // 
+            this.noTxt.Location = new System.Drawing.Point(71, 10);
+            this.noTxt.Name = "noTxt";
+            this.noTxt.Size = new System.Drawing.Size(185, 21);
+            this.noTxt.TabIndex = 3;
+            this.noTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // salTxt
+            // 
+            this.salTxt.Location = new System.Drawing.Point(71, 61);
+            this.salTxt.Name = "salTxt";
+            this.salTxt.Size = new System.Drawing.Size(185, 21);
+            this.salTxt.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Salaray";
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(421, 10);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(185, 21);
+            this.nameTxt.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(377, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "name";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(479, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 100);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.salTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.noTxt);
+            this.Controls.Add(this.empo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label empo;
+        private System.Windows.Forms.TextBox noTxt;
+        private System.Windows.Forms.TextBox salTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
