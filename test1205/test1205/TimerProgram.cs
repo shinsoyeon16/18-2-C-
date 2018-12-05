831faabc1ce49785baba3783f095116c1fc36280
shinsoyeon <shinsoyeon16@yi.ackr>
2018-12-05 15:37:13 +0900

    1205 test

---
+++ test1205/test1205/TimerProgram.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.ComponentModel;
+//using System.Timers;
+
+//namespace test1205
+//{
+//    class TimerProgram : Notifier
+//    {
+
+//        Timer timer = new Timer();
+//        private long sec;
+
+//        public long Sec
+//        {
+//            get { return sec; }
+//            set
+//            {
+//                sec = value;
+//                OnPropertyChanged("Sec");
+//            }
+//        }
+//        public bool Enabled { get; set; }
+//        public TimerProgram()
+//        {
+//            Sec = 0;
+//        }
+
+//        public void Start()
+//        {
+//            timer.Interval = 1000; //1초마다 반복되도록 설정
+//            timer.Elapsed += new ElapsedEventHandler(timer_event);
+//            timer.Start();
+//        }
+
+//        private void timer_event(object sender, ElapsedEventArgs e)
+//        {
+//            Sec += 1;
+//            throw new NotImplementedException();
+//        }
+
+//        public void Stop()
+//        {
+//            timer.Stop();
+//        }
+//    }
+//}
+
---
+++ test1205/test1205/ViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace test1205
+{
+    class ViewModel : Notifier
+    {
+        private int sec;
+        public int Sec
+        {
+            get { return sec; }
+            set
+            {
+                sec = value;
+                OnPropertyChanged("Sec");
+            }
+        }
+        public bool  StartButton { 
+            set { Timer.Start(); } }
+        public bool StopButton
+        {
+            set { Timer.Stop(); }
+        }
+        public bool ResetButton
+        {
+            set { Sec=0; }
+        }
+        public Command MyCommand { get; set; }
+        public DispatcherTimer Timer { get; set; }
+        public ViewModel()
+        {
+            Sec = 0;
+            MyCommand = new Command(ExecuteMethod);
+
+            Timer = new DispatcherTimer();
+            Timer.Tick += Timer_Tick;
+            Timer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void ExecuteMethod(object obj)
+        {
+            if ((string)obj == "start")
+            {
+                Timer.Start();
+            }
+            else if ((string)obj == "stop")
+            {
+                Timer.Stop();
+            }
+            else if ((string)obj == "reset")
+            {
+                Sec = 0;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Sec += 1;
+        }
+    }
+}
