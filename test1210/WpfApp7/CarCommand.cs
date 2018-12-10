eb85f60b3abbaeb47556c078ebc5f7b57e4c520d
shinsoyeon16/h <shinsoyeon16@yi.ac.kr>
2018-12-11 00:00:49 +0900

    1210

---
+++ test1210/WpfApp7/CarCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp7
+{
+    class CarCommand : ICommand
+    {
+        Action _action;
+
+        public CarCommand(Action action)
+        {
+            this._action = action;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke();
+        }
+    }
+}
